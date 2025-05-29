2fae866b34eb749695cba390426e07891aa4f240
bla_113 <bla_113@hotmail.com>
2025-05-29 11:19:59 -0600

    Modulo de Materias

---
+++ app/Models/Materia.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Materia extends Model
+{
+    /** @use HasFactory<\Database\Factories\MateriaFactory> */
+    use HasFactory;
+
+    protected $fillable=[
+        'codigo',
+        'nombre',
+        'creditos',
+        'requisito'
+    ];
+}
